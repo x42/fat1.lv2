7ebc8d80e04e0e32f4664ed53ebd0137fd79bb96
Robin Gareus <robin@gareus.org>
2016-08-26 14:41:53 +0200

    prepare for MOD output port events (MIDI keys: active & masked)

--- modgui/x42-autotune.js
+++ modgui/x42-autotune.js
@@ -30,34 +30,71 @@ function (event) {
 		}
 	}
 
+	function update_midi_mask (data) {
+		if (data['mode'] == 2) {
+			/* manual mode -- control input applies */
+			for (k = 0; k < 12; ++k) {
+				mask_key (k, 0);
+			}
+			return;
+		}
+		if (data['mode'] == 0 && data['mask'] == 0) {
+			/* auto mode -- control applies IFF no midi notes are set */
+			for (k = 0; k < 12; ++k) {
+				mask_key (k, 0);
+			}
+			return;
+		}
+		/* midi rulez */
+		for (k = 0; k < 12; ++k) {
+			mask_key (k, (data.mask & (1 << k)) != 0 ? -1 : 1);
+		}
+	}
+
+	function update_note_set (data) {
+		for (k = 0; k < 12; ++k) {
+			set_key (k, data.set & (1 << k));
+		}
+	}
+
 	/* top-level entry, called from mod-ui */
 	if (event.type == 'start') {
 		var ports = event.ports;
+		var data = {};
+		data.mask = 0;
+		data.mode = 0;
+		data.set = 0;
 		for (var p in ports) {
 			if (ports[p].symbol == 'mode') {
-				var pk = event.icon.find ('[mod-role=piano-keyboard]');
-				if (event.value == 1) {
-					pk.css('display', 'none');
-				} else {
-					pk.css('display', 'block');
-				}
+				data.mode = event.value;
+			}
+			else if (ports[p].symbol == 'mask') {
+				data.mask = event.value;
+			}
+			else if (ports[p].symbol == 'set') {
+				data.set = event.value;
 			}
 		}
+		update_midi_mask (data);
+		update_note_set (data);
+		var ds = event.icon.find ('[mod-role=drag-handle]');
+		ds.data ('persist', data);
 	}
 	else if (event.type == 'change') {
+		var ds = event.icon.find ('[mod-role=drag-handle]');
+		var data = ds.data ('persist');
 		if (event.symbol == 'mode') {
-			var pk = event.icon.find ('[mod-role=piano-keyboard]');
-			if (event.value == 1) {
-				pk.css('display', 'none');
-			} else {
-				pk.css('display', 'block');
-			}
-			// prepare for output params -- MIDI set keys
-			if (event.value == 2) {
-				for (k = 0; k < 12; ++k) {
-					mask_key (k, 0);
-				}
-			}
+			data.mode = event.value;
+			update_midi_mask (data);
 		}
+		else if (event.symbol == 'mask') {
+			data.mask = event.value;
+			update_midi_mask (data);
+		}
+		else if (event.symbol == 'set') {
+			data.set = event.value;
+			update_note_set (data);
+		}
+		ds.data ('persist', data);
 	}
 }
