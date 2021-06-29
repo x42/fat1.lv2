x42/fat1.lv2

commit a9abe7237848c047169057d28171cc51a5c373fc

Hide tooltips on focus-out

gui/fat1.c
@@ -393,6 +393,16 @@ static void ttip_handler (RobWidget* rw, bool on, void *handle) {
 	}
 }
 
+static void
+top_leave_notify (RobWidget* rw)
+{
+	Fat1UI* ui = (Fat1UI*)rw->children[1]->top;
+	if (ui->ctbl->expose_event != rcontainer_expose_event) {
+		ui->ctbl->expose_event    = rcontainer_expose_event;
+		ui->ctbl->parent->resized = TRUE; //full re-expose
+		queue_draw (ui->rw);
+	}
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -1152,6 +1162,7 @@ static RobWidget* toplevel (Fat1UI* ui, void* const top) {
 	/* top-level packing */
 	rob_hbox_child_pack (ui->rw, ui->m0, TRUE, TRUE);
 	rob_hbox_child_pack (ui->rw, ui->ctbl, FALSE, TRUE);
+	robwidget_set_leave_notify(ui->rw, top_leave_notify);
 	return ui->rw;
 }
 

commit d877697e679f1e4f0c37c245d934969ef5c6d731

Don't hide keysel overlay on focus out
It should remain visible after mouse events, otherwise
it's almost unusible.

Fixes: a9abe7237848c047169057d28171cc51a5c373fc

gui/fat1.c
@@ -393,11 +393,16 @@ static void ttip_handler (RobWidget* rw, bool on, void *handle) {
 	}
 }
 
+
+static bool keysel_overlay (RobWidget* rw, cairo_t* cr, cairo_rectangle_t* ev);
+
+
 static void
 top_leave_notify (RobWidget* rw)
 {
 	Fat1UI* ui = (Fat1UI*)rw->children[1]->top;
-	if (ui->ctbl->expose_event != rcontainer_expose_event) {
+	if (ui->ctbl->expose_event != rcontainer_expose_event
+	    && ui->ctbl->expose_event != keysel_overlay) {
 		ui->ctbl->expose_event    = rcontainer_expose_event;
 		ui->ctbl->parent->resized = TRUE; //full re-expose
 		queue_draw (ui->rw);